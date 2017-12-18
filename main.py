7cf9c874cde32cd0037572317d6b3a3d8a06db07
Philip Du Toit <philipdutoit@yahoo.com>
2017-12-18 16:30:46 +0200

    Added interest of time api call from google

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pprint
 import environment
+import database
+import trends
+import time
 
 from googleapiclient.discovery import build
 from flask import Flask, render_template
@@ -18,6 +21,30 @@ def main():
       cx=environment.cxId,
     ).execute()
     return render_template('index.html', data=res)
+
+
+@app.route('/trends')
+def trending():
+    iotData = trends.interestOverTime("Russian Bear Vodka")
+    values = []
+    data = {}
+    dates = []
+
+    data = iotData.groupby(iotData.index.date).sum()
+    for index, row in data.iterrows():
+        dates.append(index.strftime("%Y-%m-%d"))
+        values.append(row[0])
+
+    return render_template('trends.html', values=values, dates=dates, data=data, term="Russian Bear Vodka")
+
+
+@app.route('/read')
+def read():
+    conn = database.connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM public.community where brand = 'REMY_MARTIN' limit 10")
+    rows = cur.fetchall()
+    return render_template('read.html', data=rows, length=len(rows))
 
 
 if __name__ == '__main__':
---
+++ trends.py
@@ -0,0 +1,15 @@
+from pytrends.request import TrendReq
+
+
+def interestOverTime(value):
+    pytrend = TrendReq()
+    kw_list = [value]
+    pytrend.build_payload(kw_list, cat=0, geo='ZA', gprop='', timeframe='now 7-d')
+    interest_over_time_df = pytrend.interest_over_time()
+    return interest_over_time_df
+
+
+def interestByRegion():
+    pytrend = TrendReq()
+    interest_by_region_df = pytrend.interest_by_region()
+    return interest_by_region_df
